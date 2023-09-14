885119bf678180b9b06dabb1b21b1b0e5f714707
Vijayabaskar <2020itc040@maduracollege.edu.in>
2023-09-14 21:09:40 +0530

    dark mode

---
+++ resumetailwind/script.js
@@ -0,0 +1,46 @@
+emailjs.init("5whAoyRT_fKNTIC-x");
+
+// Assuming you have form fields with ids "name", "email", "message", and a submit button with id "submit-btn"
+document
+  .getElementById("submit-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+    const alert = document.querySelector("#alert");
+    const alertempty = document.querySelector("#alertempty");
+
+
+    if (name.value != "" && email.value != "" && message.value != "") {
+      emailjs
+        .send("service_zdzgfkl", "template_6a00fcm", {
+          from_name: name.value,
+          from_email: email.value,
+          message: message.value,
+        })
+        .then(
+          function (response) {
+            console.log("Email sent:", response);
+            name.value = " ";
+            email.value = " ";
+            message.value = " ";
+
+            alert.classList.remove("hidden");
+            setTimeout(() => {
+              alert.classList.add("hidden");
+            }, 3000);
+          },
+          function (error) {
+            console.log("Email error:", error);
+            alert("Email could not be sent. Please try again later.");
+          }
+        );
+    } else {
+      alertempty.classList.remove("hidden");
+            setTimeout(() => {
+              alertempty.classList.add("hidden");
+            }, 3000);
+    }
+  });
